c6a0b4a591c4422754bb73c0eac670ced46073ba
hoangnb <hoangnb@gearinc.com>
2021-01-20 10:16:17 +0700

    Debug heroku

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.use(function (err, req, res, next) {
 
 app.listen(port, () => {
   console.log("process.env.PORT " + process.env.PORT)
+  console.log("process.env.PORT " + process.env)
+  console.log("process.env.PORT " + process)
   console.log(`Example app listening at http://localhost:${port}`);
 });
 

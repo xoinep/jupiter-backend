a2b609ed3eda8f0f213da9d64fe457b01a2c978c
hoangnb <hoangnb@gearinc.com>
2021-02-09 14:10:25 +0700

    Added logic for medicine

--- models/wallet/wallet.types.js
+++ models/wallet/wallet.types.js
@@ -3,7 +3,7 @@ const walletTypes = {};
 walletTypes.Fix = () => {
   return {
     FOOD: "kg",
-    MEDICINE: "kg",
+    MEDICINE: "kg/l",
     EQUIPMENT: "unit",
     CLEANING: "vnd",
     LABOR: "vnd",

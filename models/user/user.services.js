66135669d5a7118ab06942bc46e5816fa71f8915
hoangnb <hoangnb@gearinc.com>
2021-02-17 17:42:16 +0700

    Added create sub account flow

--- models/user/user.services.js
+++ models/user/user.services.js
@@ -6,33 +6,44 @@ const jwt = require('../../utils/jwt');
 const UserServices = {};
 
 UserServices.createUser = async (name, email, phone, location, avatar, detailInformation) => {
-  let user = await User.findOne({ email });
-  if (user) {
-    throw error(400, 'User already exists');
-  }
-  user = await User.create({ name, email, phone, location, avatar, detailInformation });
-  return await jwt.sign({ userId: user._id });
+    let user = await User.findOne({email});
+    if (user) {
+        throw error(400, 'User already exists');
+    }
+    user = await User.create({name, email, phone, location, avatar, detailInformation});
+    return await jwt.sign({userId: user._id});
 };
 
 UserServices.createSubUser = async (name, email, phone, location, avatar) => {
-  return await UserServices.createUser(name, email, phone, location, avatar, { isRoot: false });
+    return await UserServices.createUser(name, email, phone, location, avatar, {isRoot: false});
 };
 
-UserServices.loginWithUserId = async (userId) => {
+UserServices.updateSubUsers = async (userId, updatedData) => {
+  return User.findByIdAndUpdate(userId, updatedData, {new: true});
+}
+
+UserServices.findUserById = async (userId) => {
   let user = await User.findById(userId);
   if (!user) {
     throw error(404, 'User not found!');
   }
+  return user;
+}
+UserServices.loginWithUserId = async (userId) => {
+    let user = await User.findById(userId);
+    if (!user) {
+        throw error(404, 'User not found!');
+    }
 
-  return await jwt.sign({ userId: user._id });
+    return await jwt.sign({userId: user._id});
 };
 
 UserServices.loginWithGoogle = async (email) => {
-  let user = await User.findOne({ email });
-  if (!user) {
-    throw error(404, 'User not found!');
-  }
-  return await jwt.sign({ userId: user._id });
+    let user = await User.findOne({email});
+    if (!user) {
+        throw error(404, 'User not found!');
+    }
+    return await jwt.sign({userId: user._id});
 };
 
 module.exports = UserServices;
--- routes/user.js
+++ routes/user.js
@@ -3,62 +3,70 @@ const router = express.Router();
 const userServices = require("../models/user/user.services");
 const areaServices = require("../models/area/area.services");
 const poolServices = require("../models/pool/pool.services");
-const walletServices = require("../models/wallet/wallet.services");
-const transactionService = require("../models/transaction/transaction.services");
 const checkLoggedIn = require("../utils/checkLoggedIn.middleware")
-const moment = require("moment");
+const jwt = require("../utils/jwt");
+const User = require("../models/user/user.model");
 
-router.get("/get", checkLoggedIn,  async (req, res) => {
-  /* 	#swagger.tags = ['User']
-        #swagger.description = 'Get user information'
-        #swagger.security = [{
-            "access_token": []
-        }]
-  */
+router.get("/get", checkLoggedIn, async (req, res) => {
+    /* 	#swagger.tags = ['User']
+          #swagger.description = 'Get user information'
+          #swagger.security = [{
+              "access_token": []
+          }]
+    */
 
-  const { userId } = req;
-  console.log(userId)
-  // Get user's Areas
-  let areas = await areaServices.getAreasByOwnerId(userId);
+    const {userId} = req;
+    console.log(userId)
+    let user = await userServices.findUserById(userId);
+    // Get user's Areas
+    let areas = await areaServices.getAreasByOwnerId(userId);
 
-  let areaIds = [];
-  areas.forEach((element) => {
-    areaIds.push(element._id);
-  });
+    let areaIds = [];
+    areas.forEach((element) => {
+        areaIds.push(element._id);
+    });
 
-  let poolIds = [];
-  let pools = await poolServices.getPoolsByAreaIds(areaIds);
+    let poolIds = [];
+    let pools = await poolServices.getPoolsByAreaIds(areaIds);
 
-  pools.forEach((e) => {
-    poolIds.push(e._id);
-  });
+    pools.forEach((e) => {
+        poolIds.push(e._id);
+    });
 
-  // Format data
-  let result = {
-    areas,
-    pools,
-  }
-  console.log(`userId ${userId} data ${JSON.stringify(result)}`)
-  res.status(200).send(result);
+    // Format data
+    let result = {
+        areas,
+        pools,
+        user
+    }
+    console.log(`userId ${userId} data ${JSON.stringify(result)}`)
+    res.status(200).send(result);
 });
 
-router.post("/create-user", async(req, res) => {
-  /* 	#swagger.tags = ['User']
-        #swagger.description = 'create sub user ' */
+router.post("/create-user", async (req, res) => {
+    /* 	#swagger.tags = ['User']
+          #swagger.description = 'create sub user ' */
 
-  /*	#swagger.parameters['obj'] = {
-            in: 'body',
-            description: 'Create a new sub user',
-            required: true,
-            type: 'object',
-            schema: { $ref: "#/definitions/createUserRequest" }
-    } */
-  try {
-    let token = await userServices.createSubUser(req.body)
-    res.send(token);
-  }catch (e) {
-    res.status(e.status).send(e.message);
-  }
+    /*	#swagger.parameters['obj'] = {
+              in: 'body',
+              description: 'Create a new sub user',
+              required: true,
+              type: 'object',
+              schema: { $ref: "#/definitions/createUserRequest" }
+      } */
+    try {
+        const {name, email, phone, location} = req.body.payload;
+        const userId = req.userId;
+        let user = await  userServices.findUserById(userId);
+        let detailInformation = user.detailInformation;
+        let token = await userServices.createSubUser(name, email, phone, location, null)
+        let subAccountId = jwt.decode(token);
+        detailInformation.subAccounts.push({subAccountId: subAccountId.userId, subAccountName: name});
+        user = await  userServices.updateSubUsers(userId, {detailInformation: detailInformation});
+        res.send(user);
+    } catch (e) {
+        res.status(e.status).send(e.message);
+    }
 
 
 })
